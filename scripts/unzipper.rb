8354e08b54279f1e7ee27dff605141653075baba
thdaraujo <thd.araujo@gmail.com>
2015-12-22 22:34:14 -0200

    adjusts

--- scripts/unzipper.rb
+++ scripts/unzipper.rb
@@ -6,10 +6,6 @@ require 'zlib'
 
 #example: ./unzipper.rb /media/thiago/data/lattes/0.tar 0/2137075717847290.zip ../cvs/
 
-
-tar_cmd = "tar -xvf /media/thiago/data/lattes/0.tar 0/2137075717847290.zip -C ../cvs/"
-zip_cmd = "unzip ../cvs/0/2137075717847290.zip"
-puts tar_cmd
-exec tar_cmd
-puts zip_cmd
-exec zip_cmd
+`tar -xvf /media/thiago/data/lattes/0.tar 0/1419722196483320.zip`
+`unzip 0/\*.zip -d ../cvs`
+`rm -rf 0/` #TODO nao deveria criar esse diretorio 0/
