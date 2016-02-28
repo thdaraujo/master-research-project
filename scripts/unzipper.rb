2c3a92f570be5560a72afb2864a5865ad1de0866
Thiago Araujo <worf.mogh@gmail.com>
2016-02-28 10:13:19 -0300

    ajuste

--- scripts/unzipper.rb
+++ scripts/unzipper.rb
@@ -6,6 +6,15 @@ require 'zlib'
 
 #example: ./unzipper.rb /media/thiago/data/lattes/0.tar 0/2137075717847290.zip ../cvs/
 
-`tar -xvf /media/thiago/data/lattes/0.tar 0/1419722196483320.zip`
-`unzip 0/\*.zip -d ../cvs`
-`rm -rf 0/` #TODO nao deveria criar esse diretorio 0/
+# `tar -xvf /media/thiago/data/lattes/0.tar 0/1419722196483320.zip`
+`tar -xvf /media/thiago/data/lattes/0.tar --strip-components=1`
+`unzip \*.zip -d ../cvs`
+
+
+
+for f in *.zip; do
+  dir=${f%.zip}
+
+  unzip -d "./cvs/$dir" "./$f"
+  mv "./cvs/$dir/curriculo.xml" "./temp/$dir.xml"
+done
